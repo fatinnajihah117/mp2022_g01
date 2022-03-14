048934ace982ef6a7f784ba765747dc311cc4fba
clearwing7718 <99313368+clearwing7718@users.noreply.github.com>
2022-03-14 21:51:48 +0800

    Forgot Password

--- app/src/main/java/com/example/mp2022_g01/MainActivity.java
+++ app/src/main/java/com/example/mp2022_g01/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private int counter = 5;
     private TextView userRegistration;
+    private TextView forgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Info = (TextView) findViewById(R.id.tvInfo);
         Login = (Button) findViewById(R.id.btnLogin);
         userRegistration = (TextView)findViewById(R.id.tvRegistration);
-
+        forgetPassword = (TextView)findViewById(R.id.tvForgotPassword);
         Info.setText("No of attempts remaining: 5");
 
         Login.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +46,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
             }
         });
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ForgotPassword.class));
+            }
+        });
+
     }
 
     private void validate(String userName, String userPassword) {
